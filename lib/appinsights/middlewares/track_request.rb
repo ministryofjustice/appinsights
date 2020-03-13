d63938ff38745e180e903a3e8bbe86932979a3de
Stephen Dicks <stephen@starswan.com>
2020-03-13 14:44:57 +0000

    converted ignore_prefixes to prefixes_to_ignore

--- lib/appinsights/middlewares/track_request.rb
+++ lib/appinsights/middlewares/track_request.rb
@@ -2,15 +2,15 @@ require 'application_insights'
 
 module AppInsights
   class TrackRequest
-    def initialize(app, instrumentation_key, ignore_prefixes = [], buffer_size = 500, send_interval = 60)
+    def initialize(app, instrumentation_key, prefixes_to_ignore = [], buffer_size = 500, send_interval = 60)
       @app = app
-      @ignore_prefixes = ignore_prefixes
+      @prefixes_to_ignore = prefixes_to_ignore
       @track_request = ApplicationInsights::Rack::TrackRequest.new(app, instrumentation_key, buffer_size, send_interval)
     end
 
     def call(env)
       path_info = env['PATH_INFO']
-      if @ignore_prefixes.any? { |prefix| path_info.start_with?(prefix) }
+      if @prefixes_to_ignore.any? { |prefix| path_info.start_with?(prefix) }
         @app.call env
       else
         @track_request.call env
